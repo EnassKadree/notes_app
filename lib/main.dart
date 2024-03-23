4edfee03aa32ccd7217b39f350b3d8cdf8103e4a
enasskadree2003 <enasskadree2003@gmail.com>
2024-03-24 02:26:47 +0300

    provide add note cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:ntoes_app/constants.dart';
+import 'package:ntoes_app/cubits/add%20note%20cubit/add_note_cubit.dart';
 import 'package:ntoes_app/models/note.dart';
 import 'package:ntoes_app/views/notes_view.dart';
+import 'package:bloc/bloc.dart';
 
-void main() async
-{
+void main() async {
   await Hive.initFlutter();
   await Hive.openBox(kNoteBox);
   Hive.registerAdapter(NoteAdapter());
   runApp(const NotesApp());
 }
 
-class NotesApp extends StatelessWidget 
-{
+class NotesApp extends StatelessWidget {
   const NotesApp({super.key});
 
   @override
-  Widget build(BuildContext context) 
-  {
-    return MaterialApp
+  Widget build(BuildContext context) {
+    return MultiBlocProvider
     (
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData
-      (
-        brightness: Brightness.dark, 
-        fontFamily: 'Poppins',
-        floatingActionButtonTheme: const FloatingActionButtonThemeData
+      providers: 
+      [
+        BlocProvider
         (
-          backgroundColor: kPrimaryColor,
-          shape: CircleBorder(),
-        )
-      ),
-      home: const NotesView(),
+          create: (context) => AddNoteCubit(),
+        ),
+      ],
+      child: MaterialApp
+      (
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+            brightness: Brightness.dark,
+            fontFamily: 'Poppins',
+            floatingActionButtonTheme: const FloatingActionButtonThemeData(
+              backgroundColor: kPrimaryColor,
+              shape: CircleBorder(),
+            )),
+        home: const NotesView(),
+      )
     );
   }
 }
