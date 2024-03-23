4597eeeb031c94b85cdbb0f3dcba5d3d9cc33b24
enasskadree2003 <enasskadree2003@gmail.com>
2024-03-23 04:25:05 +0300

    create note model

---
+++ lib/models/note.dart
@@ -0,0 +1,9 @@
+class Note
+{
+  final String title; 
+  final String content; 
+  final String date;
+  final int color;
+
+  Note({required this.title, required this.content, required this.date, required this.color}); 
+}
