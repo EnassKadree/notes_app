01803bd957fb8d637d33ad380728677d02d5c14c
enasskadree2003 <enasskadree2003@gmail.com>
2024-03-24 03:30:37 +0300

    refactor bloc provider

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:ntoes_app/constants.dart';
-import 'package:ntoes_app/cubits/add%20note%20cubit/add_note_cubit.dart';
 import 'package:ntoes_app/models/note.dart';
 import 'package:ntoes_app/simple_bloc_observer.dart';
 import 'package:ntoes_app/views/notes_view.dart';
@@ -12,8 +11,8 @@ void main() async
 {
   Bloc.observer = SimpleBlocObserver();
   await Hive.initFlutter();
-  await Hive.openBox<Note>(kNoteBox);
   Hive.registerAdapter(NoteAdapter());
+  await Hive.openBox<Note>(kNoteBox);
   runApp(const NotesApp());
 }
 
@@ -22,27 +21,17 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider
+    return  MaterialApp
     (
-      providers: 
-      [
-        BlocProvider
-        (
-          create: (context) => AddNoteCubit(),
-        ),
-      ],
-      child: MaterialApp
-      (
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-            brightness: Brightness.dark,
-            fontFamily: 'Poppins',
-            floatingActionButtonTheme: const FloatingActionButtonThemeData(
-              backgroundColor: kPrimaryColor,
-              shape: CircleBorder(),
-            )),
-        home: const NotesView(),
-      )
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          brightness: Brightness.dark,
+          fontFamily: 'Poppins',
+          floatingActionButtonTheme: const FloatingActionButtonThemeData(
+            backgroundColor: kPrimaryColor,
+            shape: CircleBorder(),
+          )),
+      home: const NotesView(),
     );
   }
 }
--- lib/views/widgets/add_note_modal.dart
+++ lib/views/widgets/add_note_modal.dart
@@ -4,38 +4,32 @@ import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:ntoes_app/cubits/add%20note%20cubit/add_note_cubit.dart';
 import 'package:ntoes_app/views/widgets/add_note_form.dart';
 
-class AddNoteModalBottomSheet extends StatelessWidget 
-{
+class AddNoteModalBottomSheet extends StatelessWidget {
   const AddNoteModalBottomSheet({super.key});
 
   @override
-  Widget build(BuildContext context) 
-  {
-    return BlocConsumer<AddNoteCubit, AddNoteState>
-    (
-      listener: (context, state) 
-      {
-        if(state is AddNoteFailures)
-        {
-          print('there was an error ${state.errMessage}');
-        }
-        else if(state is AddNotesSuccess)
-        {
-          Navigator.of(context).pop(context);
-        }
-      },
-      builder: (context, state) 
-      {
-        return ModalProgressHUD
-        (
-          inAsyncCall: state is AddNoteLoading ? true : false,
-          child: const Padding
-          (
-            padding: EdgeInsets.symmetric(vertical: 32, horizontal: 16),
-            child: AddNoteForm(),
-          ),
-        );
-      },
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => AddNoteCubit(),
+      child: BlocConsumer<AddNoteCubit, AddNoteState>
+      (
+        listener: (context, state) {
+          if (state is AddNoteFailures) {
+            print('there was an error ${state.errMessage}');
+          } else if (state is AddNotesSuccess) {
+            Navigator.of(context).pop(context);
+          }
+        },
+        builder: (context, state) {
+          return ModalProgressHUD(
+            inAsyncCall: state is AddNoteLoading ? true : false,
+            child: const Padding(
+              padding: EdgeInsets.symmetric(vertical: 32, horizontal: 16),
+              child: AddNoteForm(),
+            ),
+          );
+        },
+      ),
     );
   }
 }
